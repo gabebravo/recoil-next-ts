766fa759e3ad8f678d1262ade57f0675e2fa0e61
Gabe Bravo <gabecbravo@gmail.com>
2022-07-14 17:11:34 -0400

    Added the selector family for individual pokemon async data.

--- pages/pokemon-api/[pokemonName].tsx
+++ pages/pokemon-api/[pokemonName].tsx
@@ -1,16 +1,41 @@
-import { Text } from '@chakra-ui/react';
+import { Box, List, ListItem, Text } from '@chakra-ui/react';
+import { useRecoilValueLoadable } from 'recoil';
 import { useRouter } from 'next/router'; // custom hook to get route params
 import BackLink from '../components/BackLink';
+import { PokemonState } from './pokemonSelectors';
+
+const Pokemon = ({ name }: { name: string }) => {
+  const pokemonData = useRecoilValueLoadable(PokemonState(name));
+  const { base_experience, height, weight, sprites } = pokemonData.contents;
+
+  switch (pokemonData.state) {
+    case 'hasValue':
+      return (
+        <Box>
+          <img src={sprites.front_default} alt={`${name} pokemon image`} />
+          <List>
+            <ListItem>{`Experience: ${base_experience}`}</ListItem>
+            <ListItem>{`Height: ${height}`}</ListItem>
+            <ListItem>{`Weight: ${weight}`}</ListItem>
+          </List>
+        </Box>
+      );
+    case 'loading':
+      return <div>Loading...</div>;
+    case 'hasError':
+      throw pokemonData.contents;
+  }
+};
 
 const PokemonPage = () => {
   const router = useRouter(); // route object with all the data
   const pokemonName = router.query.pokemonName;
-  console.log('IN HERE', pokemonName);
 
   return (
     <div>
       <BackLink url="/pokemon-api" />
       <Text fontSize={24}>{pokemonName}</Text>
+      <Pokemon name={String(pokemonName)} />
     </div>
   );
 };
--- pages/pokemon-api/pokemonSelectors.ts
+++ pages/pokemon-api/pokemonSelectors.ts
@@ -1,22 +1,27 @@
 import { selector, selectorFamily } from 'recoil';
 import { PokemonType } from './types';
 
-// export const PokemonListState = selectorFamily({
-//   key: 'pokemonState',
-//   get: () => async () => {
-//     const pokemonList = await fetch(
-//       'https://pokeapi.co/api/v2/pokemon?limit=10'
-//     ).then((res) => res.json());
-//     return pokemonList;
-//   },
-// });
+// SEE THIS LINK ABOUT USING ASYNC SELECTORS WITH NEXT JS : using useRecoilValueLoadable hook
+// https://github.com/facebookexperimental/Recoil/issues/722
 
-export const PokemonListState = selector({
+// simple selector for READ-ONLY async state that has no React prop value dependency to make the api calls
+export const PokemonListState = selector<{ results: PokemonType[] }>({
   key: 'PokemonListState',
   get: async () => {
     const pokemonList = await fetch(
       'https://pokeapi.co/api/v2/pokemon?limit=10'
     ).then((res) => res.json());
     return pokemonList;
+  },
+});
+
+// selectorFamily for READ-ONLY async state because there is a React prop value dependency to make the api calls
+export const PokemonState = selectorFamily<PokemonType, string>({
+  key: 'pokemonState',
+  get: (pokemonName: string) => async () => {
+    const pokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
+    ).then((res) => res.json());
+    return pokemon;
   },
 });
